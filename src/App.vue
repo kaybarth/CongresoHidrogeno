c00d0505a3856cc28dd6a3b75f85452f45c01b2f
kareem dell <kaybarth.test@gmail.com>
2025-03-23 23:49:00 -0600

    STYLE: Navbar fixed

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,31 @@ import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <div>
-    <Navbar />
-    <router-view />
+  <div class="app-container">
+    <Navbar class="navbar-fixed" />
+    <main class="main-content">
+      <router-view />
+    </main>
   </div>
 </template>
 
 <style>
+.app-container {
+  min-height: 100vh;
+}
+
+.navbar-fixed {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+}
+
+.main-content {
+  padding-top: 60px; /* Ajusta este valor según la altura de tu Navbar */
+}
+
 .container {
   max-width: 1200px;
 }
