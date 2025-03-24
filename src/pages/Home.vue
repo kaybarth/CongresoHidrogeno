b44c7c2b75d637b1961aab75569b5d118f71585b
kareem dell <kaybarth.test@gmail.com>
2025-03-23 21:34:07 -0600

    FEAT: Home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Header from '../components/Header.vue'
+import EventDetails from '../components/EventDetails.vue'
+import ProminentMembers from '../components/ProminentMembers.vue'
+</script>
+
+<template>
+  <div class="container mx-auto px-4 py-8">
+    <Header 
+      title="We invite you to the XXV Hydrogen Congress!"
+      subtitle="Join us in Morelia, Michoacán in 2025 to be part of the energy revolution that is shaping the future. Explore, learn, and connect with global leaders in the field of hydrogen energy."
+    />
+    <EventDetails />
+    <ProminentMembers />
+  </div>
+</template>
